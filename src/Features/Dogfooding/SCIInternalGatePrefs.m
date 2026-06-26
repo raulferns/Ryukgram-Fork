2fdcd739c04a484c4358166f3abf70241ea6652c
RaulTheMiner17 <raulferns1@gmail..com>
2026-06-26 21:06:31 +0530

    Fix sandbox violation crash on launch/shake by resolving container caches directory properly

--- src/Features/Dogfooding/SCIInternalGatePrefs.m
+++ src/Features/Dogfooding/SCIInternalGatePrefs.m
@@ -115,7 +115,8 @@ static void SCIReinstallCrashSignalHandlers(void) {
 static void SCIInstallCrashSignalHandlers(void) {
     static dispatch_once_t once;
     dispatch_once(&once, ^{
-        NSString *path = [[NSHomeDirectory() stringByAppendingPathComponent:@"Library/Caches"] stringByAppendingPathComponent:@"ryukgram_internal_gate_crash.marker"];
+        NSString *cachesDir = [NSSearchPathForDirectoriesInDirectories(NSCachesDirectory, NSUserDomainMask, YES) firstObject];
+        NSString *path = [cachesDir stringByAppendingPathComponent:@"ryukgram_internal_gate_crash.marker"];
         const char *fs = path.fileSystemRepresentation;
         if (fs) strncpy(gSCICrashMarkerPath, fs, sizeof(gSCICrashMarkerPath) - 1);
         SCIReinstallCrashSignalHandlers();
